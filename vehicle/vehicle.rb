3bb59820248b56224b23b64f6aeacdbebec22481
Julian Stasiak <stasiakjulian@gmail.com>
2022-11-09 19:14:33 +0000

    dodano druga czesc cwiczenia z klasami + dorzucilem wylapywanie wyjatkow

--- vehicle/vehicle.rb
+++ vehicle/vehicle.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+
 class Vehicle
     attr_accessor :open_doors_count
 
@@ -12,6 +14,8 @@ class Vehicle
         else
             raise Exception, "Cant open any more doors"
         end
+    rescue Exception => e
+        puts "ERROR: #{self.class}" + " " + e.to_s
     end
 
     def close_door
@@ -21,11 +25,13 @@ class Vehicle
         else
             raise Exception, "Cant close any more doors"
         end
+    rescue Exception => e
+        puts "ERROR: #{self.class}" + " " + e.to_s
     end
 
     private
     def print_current_count
-        puts "Doors currently open #{open_doors_count}"
+        puts "#{self.class} Doors currently open #{open_doors_count}"
     end
 
 end
@@ -41,7 +47,15 @@ end
 class Coach < Vehicle
 
     def doors_count
-        4
+        6
     end
 
 end
+
+Seat = PassengerCar.new
+Volvo = Coach.new
+Seat.open_door
+Volvo.close_door
+
+6.times {Volvo.open_door}
+5.times {Seat.open_door}
