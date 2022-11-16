e5fbe81b3b1af0870983327c5fc57302834b7d46
Julian Stasiak <stasiakjulian@gmail.com>
2022-11-16 16:49:38 +0000

    User db changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: "posts#index"
+
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
